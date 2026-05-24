0f8bb451410b7b2d13df322b3cf50e119cedea6e
davdprad <davidprado0113@gmail.com>
2026-05-24 12:54:31 -0400

    feat: conversao errada

--- app/converter.py
+++ app/converter.py
@@ -2,8 +2,8 @@ def f_to_c(fahrenheit):
     """Converte Fahrenheit para Celsius"""
     return (fahrenheit - 32) * 5 / 9
 
-def c_to_f(celsius)
+def c_to_f(celsius):
     """Converte Celsius para Fahrenheit"""
-    return (celsius * 9 / 5) + 100
+    return (celsius * 9 / 5) + 32
 
 # Agora o build dá certo mas o teste não passa
