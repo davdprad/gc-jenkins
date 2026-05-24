1077b399ef7a3c3a75f62064513e76d465a57f98
davdprad <davidprado0113@gmail.com>
2026-05-24 13:00:42 -0400

    feat: trigger de 1 em 1 min

--- app/converter.py
+++ app/converter.py
@@ -4,6 +4,4 @@ def f_to_c(fahrenheit):
 
 def c_to_f(celsius):
     """Converte Celsius para Fahrenheit"""
-    return (celsius * 9 / 5) + 100
-
-# Agora o build dá certo mas o teste não passa
+    return (celsius * 9 / 5) + 32
